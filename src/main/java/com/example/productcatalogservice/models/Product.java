575abd207cd901e05fd7659ddf285ac6f5a52800
Ankit Negi <ankitnegi1155@gmail.com>
2026-08-18 10:57:37 +0530

    Created Models and Controllers

---
+++ src/main/java/com/example/productcatalogservice/models/Product.java
@@ -0,0 +1,14 @@
+package com.example.productcatalogservice.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Product extends BaseModel{
+    private String name;
+    private String description;
+    private Double price;
+    private String imageUrl;
+    private Category category;
+}
